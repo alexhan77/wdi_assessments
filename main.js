082e03b432e34d200d7d9616c8dd8890d044c7ba
Jummay77 <hansangho@yahoo.com>
2020-04-06 18:24:36 -0700

    finalized assessment

--- main.js
+++ main.js
@@ -1,32 +1,58 @@
-var x = document.getElementById('number').value
-var y = document.getElementById('results').value
+var sub = document.getElementById('subtract')
+var add = document.getElementById('add')
+var num = document.getElementById('input')
 
+var display = document.getElementById('display')
+var total = 0
+// displayNumber functions 
 
-function displayNumber()
-{
-    var z = document.getElementById('results').innerHTML;
-    document.getElementById('results').innerHTML = 0;
-}
+// subtract button function
+// subtract the input number from the display number 
+// add button function
 
-function incrementValue()
-{
-    var value = parseInt(document.getElementById('number').value, 0);
-    // value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-    // document.getElementById('number').addEventListener('click', incrementValue)
-    
-    // console.log(value) 
+function subtract() {
+
+    total = total - num.value
+    // console.log(total)
+    display.innerHTML = total
+
 }
-function decrementValue()
-{
-    var value = parseInt(document.getElementById('number').value, 0);
-    // value = isNaN(value) ? 0 : value;
-    value--;
-    document.getElementById('number').value = value;
-    // document.getElementById('number').addEventListener('click', decrementValue)
-    // console.log(value)
+sub.addEventListener('click', subtract)
+
+function addition() {
+
+    total = total + parseInt(num.value)
+    // console.log(total)
+    display.innerHTML = total
 }
-// decrementValue();
-// incrementValue();
-displayNumber();
+add.addEventListener('click', addition)
+
+// function displayNumber()
+// {
+//     var z = document.getElementById('results').innerHTML;
+//     document.getElementById('results').innerHTML = 0;
+// }
+
+// function incrementValue()
+// {
+//     var value = parseInt(document.getElementById('number').value, 0);
+//     // value = isNaN(value) ? 0 : value;
+//     value++;
+//     document.getElementById('number').value = value;
+//     // document.getElementById('number').addEventListener('click', incrementValue)
+
+//     // console.log(value) 
+// }
+// function decrementValue()
+// {
+//     var value = parseInt(document.getElementById('number').value, 0);
+//     // value = isNaN(value) ? 0 : value;
+//     value--;
+//     document.getElementById('number').value = value;
+//     // document.getElementById('number').addEventListener('click', decrementValue)
+//     // console.log(value)
+// }
+// // decrementValue();
+// // incrementValue();
+// displayNumber();
+
